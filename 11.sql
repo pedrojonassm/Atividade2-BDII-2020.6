607c369304c8ff7ab64200e2e768b2bac4ed9878
pedrojonassm <pedrojonassm@gmail.com>
2020-10-06 11:57:07 -0300

    Questão 11 adicionada e testada

---
+++ 11.sql
@@ -0,0 +1,15 @@
+/*
+Faça uma consulta que selecione o nome do projeto, o nome do departamento do projeto,
+o nome do funcionário responsável pelo projeto, o nome do departamento do funcionário
+responsável, mas apenas os projetos que o responsável é de outro departamento.
+*/
+
+select proj.descricao as "nome do projeto", d.descricao as "departamento do projeto",
+f.nome "nome do responsavel", d2.descricao "departamento do funcionario"
+from projeto proj left join departamento d
+on proj."codDep" = d.codigo
+left join funcionario f
+on f.codigo = proj."codResp"
+left join departamento d2
+on f."codDep" = d2.codigo
+where proj."codDep" != f."codDep";
